7962babd7ddd58d12188c2e7ce2a023dc30b3835
Shyam sundar R <shyamsundarr10@gmail.com>
2023-08-15 00:03:03 +0530

    emails page is created and check

---
+++ src/components/Emails.js
@@ -0,0 +1,15 @@
+import { useOutletContext } from "react-router-dom";
+
+const Emails = () => {
+  const { openDrawer } = useOutletContext();
+
+  return (
+    <div
+      style={openDrawer ? { marginLeft: 250, width: "75%" } : { width: "100%" }}
+    >
+      Emails
+    </div>
+  );
+};
+
+export default Emails;
