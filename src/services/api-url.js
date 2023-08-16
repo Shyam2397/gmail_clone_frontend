5062d719f9ace4ec5d3ca2f37461a898a507fb1e
Shyam sundar R <shyamsundarr10@gmail.com>
2023-08-16 17:51:20 +0530

    email,draft,bin,starred and delete endpoint and method

--- src/services/api-url.js
+++ src/services/api-url.js
@@ -3,4 +3,24 @@ export const API_URLS = {
     endpoint: "save",
     method: "POST",
   },
+  getEmailFromType: {
+    endpoint: "emails",
+    method: "GET",
+  },
+  saveDraftEmails: {
+    endpoint: "save-draft",
+    method: "POST",
+  },
+  moveEmailsToBin: {
+    endpoint: "bin",
+    method: "POST",
+  },
+  toggleStarredEmail: {
+    endpoint: "starred",
+    method: "POST",
+  },
+  deleteEmail: {
+    endpoint: "delete",
+    method: "DELETE",
+  },
 };
